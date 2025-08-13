9cc88d8050feecd6da3d1aaac35a9b35b4d96ca9
Ryan Do <rdo9313@gmail.com>
2025-08-13 11:19:03 -0400

    classes and objects 1 exercises

---
+++ exercises/classes_and_objects_1/ex_4.py
@@ -0,0 +1,6 @@
+class Cat:
+
+    def __init__(self):
+        print("I'm a cat!")
+
+kitty = Cat()
---
+++ exercises/classes_and_objects_1/ex_5.py
@@ -0,0 +1,6 @@
+class Cat:
+    def __init__(self, name):
+        self._name = name
+        print(f"Hello! My name is {self._name}!")
+
+kitty = Cat('Sophie')
