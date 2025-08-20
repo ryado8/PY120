376b47725e78dc72f01b89ca5ac6019d74608923
Ryan Do <kyogen12622@gmail.com>
2025-08-20 08:28:01 -0400

    fully working oo_ttt

--- lesson_5/OO_TTT.py
+++ lesson_5/OO_TTT.py
@@ -1,13 +1,30 @@
+import random
+import os
+
+def clear_screen():
+    os.system("clear")
+
 class Square:
     INITIAL_MARKER = " "
-    HUMAN_MARKER = "\u274C"
-    COMPUTER_MARKER = "\u2B55"
+    HUMAN_MARKER = "X"
+    COMPUTER_MARKER = "O"
 
     def __init__(self, marker=INITIAL_MARKER):
         self.marker = marker
 
     def __str__(self):
         return self.marker
+
+    @property
+    def marker(self):
+        return self._marker
+
+    @marker.setter
+    def marker(self, marker):
+        self._marker = marker
+
+    def is_unused(self):
+        return self.marker == Square.INITIAL_MARKER
 
 class Board:
     def __init__(self):
@@ -34,105 +51,139 @@ class Board:
         print("     |     |")
         print()
 
-class Row:
-    def __init__(self):
-        # STUB
-        # We need some way to identify a row of 3 squares
-        pass
+    def display_with_clear(self):
+        clear_screen()
+        print("\n")
+        self.display()
 
-class Marker:
-    def __init__(self):
-        # STUB
-        # A marker is something that represents a board
-        #   square that belongs to a particular player. That
-        #   is, it's a square that was chosen by the player.
-        pass
+    def count_markers_for(self, player, keys):
+        markers = [self.squares[key].marker for key in keys]
+        return markers.count(player.marker)
+
+    def mark_square_at(self, key, marker):
+        self.squares[key].marker = marker
+
+    def unused_squares(self):
+        return [key
+                for key, square in self.squares.items()
+                if square.is_unused()]
+
+    def is_full(self):
+        return len(self.unused_squares()) == 0
 
 class Player:
-    def __init__(self):
-        # STUB
-        # A player is either a human or a computer that is
-        #   playing the game.
-        # Perhaps we need a "marker" to keep track of this
-        #   player's symbol? (i.e., 'X' or 'O')
-        pass
+    def __init__(self, marker):
+        self.marker = marker
 
-    def mark(self):
-        # STUB
-        # We need a way to mark the board with this player's
-        #   marker. How do we access the board?
-        pass
+    @property
+    def marker(self):
+        return self._marker
 
-    def play(self):
-        # STUB
-        # We need a way for each player to play the game.
-        # Do we need access to the board?
-        pass
+    @marker.setter
+    def marker(self, marker):
+        self._marker = marker
 
 class Human(Player):
     def __init__(self):
-        # STUB
-        # What does a human player need to do? How does it
-        #   differ from the basic Player or a Computer?
-        pass
+        super().__init__(Square.HUMAN_MARKER)
 
 class Computer(Player):
     def __init__(self):
-        # STUB
-        # What does a computer player need to do? How does
-        #   it differ from the basic Player or a Human?
-        pass
+        super().__init__(Square.COMPUTER_MARKER)
 
 class TTTGame:
+    POSSIBLE_WINNING_ROWS = (
+        (1, 2, 3),
+        (4, 5, 6),
+        (7, 8, 9),
+        (1, 4, 7),
+        (2, 5, 8),
+        (3, 6, 9),
+        (1, 5, 9),
+        (3, 5, 7),
+    )
+
     def __init__(self):
         self.board = Board()
+        self.human = Human()
+        self.computer = Computer()
 
     def play(self):
-        # SPIKE
         self.display_welcome_message()
+        self.board.display()
 
         while True:
-            self.board.display()
-
-            self.first_player_moves()
+            self.human_moves()
             if self.is_game_over():
                 break
 
-            self.second_player_moves()
+            self.computer_moves()
             if self.is_game_over():
                 break
 
-            break
+            self.board.display_with_clear()
 
-        self.board.display()
+        self.board.display_with_clear()
         self.display_results()
         self.display_goodbye_message()
 
     def display_welcome_message(self):
+        clear_screen()
         print("Welcome to Tic Tac Toe!")
+        print()
 
     def display_goodbye_message(self):
         print("Thanks for playing Tic Tac Toe! Goodbye!")
 
     def display_results(self):
-        # STUB
-        # Show the results of this game (win, lose, tie).
-        pass
+        if self.is_winner(self.human):
+            print("You won! Congratulations!")
+        elif self.is_winner(self.computer):
+            print("I won! I won! Take that, human!")
+        else:
+            print("A tie game. How boring.")
 
-    def first_player_moves(self):
-        # STUB
-        # The first player makes a move.
-        pass
+    def is_winner(self, player):
+        for row in TTTGame.POSSIBLE_WINNING_ROWS:
+            if self.three_in_a_row(player, row):
+                return True
 
-    def second_player_moves(self):
-        # STUB
-        # The second player makes a move.
-        pass
+        return False
+
+    def human_moves(self):
+        valid_choices = self.board.unused_squares()
+        while True:
+            choices_list = [str(choice) for choice in valid_choices]
+            choices_str = ", ".join(choices_list)
+            prompt = f"Choose a square ({choices_str}): "
+            choice = input(prompt)
+
+            try:
+                choice = int(choice)
+                if choice in valid_choices:
+                    break
+            except ValueError:
+                pass
+
+            print("Sorry, that's not a valid choice.")
+            print()
+
+        self.board.mark_square_at(choice, self.human.marker)
+
+    def computer_moves(self):
+        valid_choices = self.board.unused_squares()
+        choice = random.choice(valid_choices)
+        self.board.mark_square_at(choice, self.computer.marker)
+
+    def three_in_a_row(self, player, row):
+        return self.board.count_markers_for(player, row) == 3
+
+    def someone_won(self):
+        return (self.is_winner(self.human) or
+                self.is_winner(self.computer))
 
     def is_game_over(self):
-        # STUB
-        # We'll start by assuming the game never ends.
-        return False
+        return self.board.is_full() or self.someone_won()
 
 game = TTTGame()
 game.play()
